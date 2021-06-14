756df6dab189b9cffb168059e4d78db00e5be41b
Shannon <shannon.nell92@gmail.com>
2021-06-14 11:57:29 -0400

    Test and build enemy's methods

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -14,3 +14,52 @@ test('creates an enemy object', () => {
     expect(enemy.agility).toEqual(expect.any(Number));
     expect(enemy.potion).toEqual(expect.any(Object));
 });
+
+//Enemy health
+test("gets enemy's health value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+//Enemy alive?
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+
+    expect(enemy.isAlive()).toBeFalsy();
+});
+
+//Enemy attack
+test("gets enemy's attack value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//Reduce health
+test("subtracts from enemy's health", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    const oldHealth = enemy.health;
+
+    enemy.reduceHealth(5);
+
+    expect(enemy.health).toBe(oldHealth - 5);
+
+    enemy.reduceHealth(99999);
+
+    expect(enemy.health).toBe(0);
+});
+
+//enemy description
+test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+});
